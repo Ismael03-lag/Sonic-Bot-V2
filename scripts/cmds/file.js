3b3db1a3354976a2a1ef759d96f7ad4ded113916
Hedgehog07 <botastro39@gmail.com>
2025-10-02 15:46:42 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,14 +14,14 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61578433048588"];
+    const permission = ["61578433048588", "61578281565957"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝑩𝒂𝒕𝒂𝒓𝒅 𝒊𝒏𝒅𝒊𝒈𝒏𝒆...𝒔𝒆𝒖𝒍 ミ★𝐒𝐎𝐍𝐈𝐂✄𝐄𝐗𝐄 3.0★彡 𝒑𝒆𝒖𝒕 𝒖𝒕𝒊𝒍𝒊𝒔𝒆𝒓 𝒄𝒆𝒕𝒕𝒆 𝒇𝒐𝒏𝒄𝒕𝒊𝒐𝒏...😒🔒🍀", event.threadID, event.messageID);
+      return api.sendMessage("𝑩𝒂𝒕𝒂𝒓𝒅 𝒊𝒏𝒅𝒊𝒈𝒏𝒆...𝒔𝒆𝒖𝒍 ミ★𝐒𝐎𝐍𝐈𝐂✄𝐄𝐗𝐄 3.0★彡 𝒆𝒕 Brayan Đ-Grimɱ 𝒑𝒆𝒖𝒗𝒆𝒏𝒕 𝒖𝒕𝒊𝒍𝒊𝒔𝒆𝒓 𝒄𝒆𝒕𝒕𝒆 𝒇𝒐𝒏𝒄𝒕𝒊𝒐𝒏...😒🔒🍀", event.threadID, event.messageID);
     }
 
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝖡𝖺𝗅𝖺𝗇𝖼𝖾 𝗅𝖾 𝗇𝗈𝗆 𝖽𝗎 𝖿𝗂𝖼𝗁𝗂𝖾𝗋.", event.threadID, event.messageID);
+      return api.sendMessage("⛔| 𝐈𝐧𝐜𝐨𝐫𝐫𝐞𝐜𝐭 𝐬𝐲𝐧𝐭𝐚𝐱. 𝐔𝐬𝐞 𝐅𝐢𝐥𝐞 <𝐜𝐦𝐝_𝐧𝐚𝐦𝐞>", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
