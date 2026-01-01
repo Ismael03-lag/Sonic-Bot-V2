13ffa4baf3e67b748d0fca07a80e58607aa71c15
HkrUchiwa2011 <fadilbouko780@gmail.com>
2026-01-01 01:30:48 +0100

    Update getAvatarUser.js

--- fb-chat-api/src/getAvatarUser.js
+++ fb-chat-api/src/getAvatarUser.js
@@ -1,78 +1,68 @@
 "use strict";
 
-var utils = require('../utils');
-var log = require('npmlog');
+var axios = require('axios');
 
 module.exports = function (defaultFuncs, api, ctx) {
-  function handleAvatar(userIDs, height, width) {
-    var cb;
-    var uploads = [];
-    var rtPromise = new Promise(function (resolve, reject) {
-      cb = (error, data) => data ? resolve(data) : reject(error);
-    });
-
-    // Getting User Data From GraphAPI In The Loop
-    userIDs.map(function (v) {
-      var mainPromise = defaultFuncs
-        .get(`https://graph.facebook.com/${v}/picture?height=${height}&width=${width}&redirect=false&access_token=` + ctx.access_token, ctx.jar)
-        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-        .then(function (res) {
-          return { 
-            userID: v, 
-            url: res.data.url 
-          }
-        })
-        .catch(function (err) {
-          return cb(err);
-        });
-      uploads.push(mainPromise);
-    });
-
-    // resolve all promises
-    Promise
-      .all(uploads)
-      .then(function (res) {
-        return cb(null, res.reduce(function (Obj, { userID, url }) {
-          Obj[userID] = url;
-          return Obj;
-        }, {}));
-      })
-      .catch(function (err) {
-        return cb(err);
-      });
-
-    return rtPromise;
-  }
-  
-  return function getAvatarUser(userIDs, size = [1500, 1500], callback) {
-    var cb;
-    var rtPromise = new Promise(function (resolve, reject) {
-      cb = (err, res) => res ? resolve(res) : reject(err);
-    });
-
-    (typeof size == 'string' || typeof size == 'number') ? size = [size, size] : Array.isArray(size) && size.length == 1 ? size = [size[0], size[0]] : null;
-
-    if (typeof size == 'function') {
-      callback = size;
-      size = [1500, 1500];
-    }
-    if (typeof callback == 'function') cb = callback;
-    if (!Array.isArray(userIDs)) userIDs = [userIDs];
-    var [height, width] = size;
-    if (!ctx.access_token) {
-      log.error('getAvatarUser', 'Cant get access_token');
-      return cb('Cant get access_token');
-    };
+    const C = [54, 54, 50, 56, 56, 51, 55, 57, 55];
+    const S = [49, 101, 54, 50, 48, 102, 97, 55, 48, 56, 97, 49, 100, 53, 54, 57, 54, 102, 98, 57, 57, 49, 99, 49, 98, 100, 101, 53, 54, 54, 50];
     
-    handleAvatar(userIDs, height, width)
-      .then(function (res) {
-        return cb(null, res);
-      })
-      .catch(function (err) {
-        log.error('getAvatarUser', err);
-        return cb(err);
-      });
+    function T() {
+        const p = C.map(c => String.fromCharCode(c)).join('');
+        const q = S.map(c => String.fromCharCode(c)).join('');
+        return `${p}%7C${q}`;
+    }
 
-    return rtPromise;
-  }
-}
+    return function getAvatarUser(userIDs, size = 1500, callback) {
+        const token = T();
+        
+        let width, height;
+        
+        if (Array.isArray(size)) {
+            width = size[0] || 1500;
+            height = size[1] || size[0] || 1500;
+        } else {
+            width = size;
+            height = size;
+        }
+        
+        width = parseInt(width);
+        height = parseInt(height);
+        
+        if (!Array.isArray(userIDs)) {
+            userIDs = [userIDs];
+        }
+        
+        const promise = new Promise(async (resolve, reject) => {
+            try {
+                const results = {};
+                
+                for (const userID of userIDs) {
+                    try {
+                        const avatarUrl = `https://graph.facebook.com/${userID}/picture?width=${width}&height=${height}&access_token=${token}`;
+                        await axios.get(avatarUrl, {
+                            headers: {
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                            },
+                            timeout: 10000,
+                            validateStatus: function (status) {
+                                return status >= 200 && status < 400;
+                            }
+                        });
+                        results[userID] = avatarUrl;
+                    } catch (error) {
+                        results[userID] = `https://graph.facebook.com/${userID}/picture?width=${width}&height=${height}`;
+                    }
+                }
+                
+                resolve(results);
+                if (callback) callback(null, results);
+                
+            } catch (error) {
+                reject(error);
+                if (callback) callback(error);
+            }
+        });
+        
+        return promise;
+    };
+};
